a315130027c8224634c6632f77b3ab1816f4cb6f
thisisaskari <muhammadaskari000@gmail.com>
2025-06-13 07:48:44 +0500

    Add prev/next buttons, adjust step logic in App.js

--- src/App.js
+++ src/App.js
@@ -7,23 +7,43 @@ const messages = [
 ];
 
 export default function App() {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    // alert("Previous");
+    if (step > 1) {
+      setStep((s) => s - 1);
+    }
+  }
+
+  function handleNext() {
+    // alert("Next");
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={step >= 0 ? "active" : ""}>1</div>
-        <div className={step >= 1 ? "active" : ""}>2</div>
-        <div className={step >= 2 ? "active" : ""}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
-        Step {step}: {messages[step]}
+        Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
