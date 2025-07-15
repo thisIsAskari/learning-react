6b205bb5e759ad759b54b066b20ad606cca24ea7
thisisaskari <muhammadaskari000@gmail.com>
2025-07-15 06:51:41 +0500

    71-Rendering the items List, Add initial packing list items and create Item component for rendering

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+ 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+  { id: 4, description: "Sunglasses", quantity: 1, packed: false },
+  { id: 5, description: "Laptop", quantity: 1, packed: false },
+  { id: 6, description: "Underwear", quantity: 3, packed: false },
+  { id: 7, description: "Phone", quantity: 1, packed: false },
+  { id: 8, description: "Headphones", quantity: 1, packed: false },
+  { id: 9, description: "Keyboard", quantity: 1, packed: false },
+  { id: 10, description: "Book", quantity: 1, packed: true },
+];
+
+
 export default function App() {
   return (
     <div className="app">
@@ -22,13 +37,20 @@ function Form() {
 function PackingList() {
   return <div className="list">
     <ul>
-      <li>
-        <span>🧳</span>
-        <span>Sunglasses</span>
-        <button>❌</button>
-      </li>
+      {initialItems.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
     </ul>
   </div>
+}
+
+function Item({ item }) {
+  return <li>
+    <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+      {item.quantity} {item.description}
+    </span>
+    <button>❌</button> 
+  </li>
 }
 
 function Stats() {
