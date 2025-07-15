d2c82491d542e04bc8ef70465ffaf541220adb15
thisisaskari <muhammadaskari000@gmail.com>
2025-07-15 07:09:27 +0500

    72-Building a Form and Handling Submissions, Implement form submission handling and enhance the Form component with a dropdown for item quantity selection.

--- src/App.js
+++ src/App.js
@@ -29,9 +29,22 @@ function Logo() {
 }
 
 function Form() {
-  return <div className="add-form">
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.elements);
+  }
+
+  return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your 😍 trip?</h3>
-  </div>
+    <select>
+      {Array.from({ length: 20}, (_, i) => i + 1).map(num => (
+        <option value={num} key={num}>{num}</option>
+      ))}
+    </select>
+    <input type="text" placeholder="Item..." />
+    <button>Add</button>
+  </form>
 }
 
 function PackingList() {
