7af6bea4bb524c808c39ff12d7c290e37eecf659
thisisaskari <muhammadaskari000@gmail.com>
2025-07-25 07:38:01 +0500

    73-Controlled Elemets, Enhance Form Component, Add state management for item description and quantity, and improve form submission handling with input bindings.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
- 
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -11,7 +12,6 @@ const initialItems = [
   { id: 9, description: "Keyboard", quantity: 1, packed: false },
   { id: 10, description: "Book", quantity: 1, packed: true },
 ];
-
 
 export default function App() {
   return (
@@ -29,45 +29,74 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
     console.log(e.target.elements);
+    
+
+    setDescription("");
+    setQuantity(1);
   }
 
-  return <form className="add-form" onSubmit={handleSubmit}>
-    <h3>What do you need for your 😍 trip?</h3>
-    <select>
-      {Array.from({ length: 20}, (_, i) => i + 1).map(num => (
-        <option value={num} key={num}>{num}</option>
-      ))}
-    </select>
-    <input type="text" placeholder="Item..." />
-    <button>Add</button>
-  </form>
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
 }
 
 function PackingList() {
-  return <div className="list">
-    <ul>
-      {initialItems.map((item) => (
-        <Item item={item} key={item.id} />
-      ))}
-    </ul>
-  </div>
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function Item({ item }) {
-  return <li>
-    <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-      {item.quantity} {item.description}
-    </span>
-    <button>❌</button> 
-  </li>
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
-  return <footer className="stats">
-    <em>💼 You are ready to go ✈️</em>
-  </footer>
+  return (
+    <footer className="stats">
+      <em>💼 You are ready to go ✈️</em>
+    </footer>
+  );
 }
