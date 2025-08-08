77ed624adb39432cd4ade52326761cc9077b8901
thisisaskari <muhammadaskari000@gmail.com>
2025-08-08 20:36:47 +0500

    Add item management to Form component, introducing state for items and handling item addition on form submission.

--- src/App.js
+++ src/App.js
@@ -31,6 +31,11 @@ function Logo() {
 function Form() {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -40,7 +45,7 @@ function Form() {
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
     console.log(e.target.elements);
-    
+    handleAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
