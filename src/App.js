9225e6c8e03e8b60ac122e9ac0e8c2cc2d008a29
thisisaskari <muhammadaskari000@gmail.com>
2025-08-11 07:24:27 +0500

    Refactor App component to manage item state and pass item handling functions to Form and PackingList components.

--- src/App.js
+++ src/App.js
@@ -14,11 +14,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -28,15 +34,11 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-  const [items, setItems] = useState([]);
 
-  // video 80 in progress 6:30 min to continue
-  function handleAddItems(item) {
-    setItems((items) => [...items, item]);
-  }
+
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -46,7 +48,7 @@ function Form() {
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
     console.log(e.target.elements);
-    handleAddItems(newItem);
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -76,11 +78,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
