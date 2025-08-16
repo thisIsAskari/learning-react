535fca658832d75447ebdfc1848ca760e419c563
thisisaskari <muhammadaskari000@gmail.com>
2025-08-16 15:32:48 +0500

    91-More Reusability with the "children" Props

--- src/App.js
+++ src/App.js
@@ -45,9 +45,7 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
               Previous<span>👈</span>
@@ -70,5 +68,13 @@ function Button({ textColor, bgColor, onClick, children }) {
     >
       {children}
     </button>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}:</h3> {children}
+    </div>
   );
 }
