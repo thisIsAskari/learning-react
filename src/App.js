aa865bcecc83ab031db25920ea4042321d0d602d
thisisaskari <muhammadaskari000@gmail.com>
2025-08-16 16:31:04 +0500

    92-Exercise: Accoddion Component V2

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import "./styles.css";
 const faqs = [
   {
     title: "Where are these chairs assembled?",
-    text:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
   {
     title: "How long do I have to return my chair?",
-    text:
-      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
   },
   {
     title: "Do you ship to countries outside the EU?",
-    text:
-      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-  }
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
 ];
 
 export default function App() {
@@ -27,25 +24,36 @@ export default function App() {
   );
 }
 
-function Accordion({data}) {
-  return <div className="accordion">
-    {
-      data.map((item, index) => (
-        <AccordionItem key={item.title} num={index + 1} title={item.title} text={item.text} />
-      ))
-    }
-  </div>
-}
-
-function AccordionItem({num, title, text}) {
-  const [isOpen, setIsOpen] = useState(false);
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
 
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+    <div className="accordion">
+      {data.map((item, index) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          num={index + 1}
+          title={item.title}
+          key={item.title}
+        >{item.text}</AccordionItem>
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ curOpen, onOpen, num, title, children }) {
+  const isOpen = curOpen === num;
+
+  return (
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => onOpen(curOpen === num ? null : num)}
+    >
       <p className="number">{num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
