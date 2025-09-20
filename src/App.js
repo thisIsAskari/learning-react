d6b19b8b5f63c47db10c7ed86304b15bd8baf361
thisisaskari <muhammadaskari000@gmail.com>
2025-09-20 16:30:30 +0500

    Section 10 [114 video]
    refactor(ui): simplify Box component usage by removing element prop alternative
    
    - Removed commented-out alternative usage of Box with element prop
    - Kept direct children usage for Box component to render MovieList
    - Cleaned up JSX to improve readability and maintainability
    - Updated comments to clarify usage options for Box component

--- src/App.js
+++ src/App.js
@@ -56,6 +56,18 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
+        {/* Alternative to using children 
+        Here we pass a component as a prop */}
+        {/* <Box element={<MovieList movies={movies} />} />  */}
+        {/* <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched} />
+            </>
+          }
+        /> */}
+
         <Box>
           <MovieList movies={movies} />
         </Box>
