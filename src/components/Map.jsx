316455f982d22d0e91eced2af48f17d579dd2794
Askari <muhammadaskari000@gmail.com>
2026-01-24 21:49:25 +0500

    Section 18 [230-231 videos]:
    feat: Add Map component with initial city data.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,28 +1,43 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import { useCitiesContext } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCitiesContext();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("/app/form");
-      }}
-    >
-      <h2>Map</h2>
-      <p>Latitude: {lat}</p>
-      <p>Longitude: {lng}</p>
-      <button
-        onClick={() => setSearchParams({ lat: "40.7128", lng: "-74.0060" })}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Set to New York City
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
